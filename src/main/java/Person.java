055383b088d26f06e28a9b1e18c44e03a8a4d4b6
tenzin <niznetgaming@gmail.com>
2023-05-25 14:29:09 -0400

    added files

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,49 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Person {
     private String firstName;
     private String lastName;
     private int age;
-    private List<Person> people;
 
-    public Person(String firstName, String lastName, int age, List<Person> people) {
+    public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
-        this.people = new ArrayList<Person>();
-
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public int getAge() {
         return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public List<Person> getPeople() {
-        return people;
-    }
-
-    public void setPeople(List<Person> people) {
-        this.people = people;
     }
 }
